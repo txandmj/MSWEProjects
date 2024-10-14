d95f33f66dfe55f72ef36095f57525ebf8907466
txandmj <txandmj@outlook.com>
2024-10-13 20:36:45 -0700

    WIP

--- Permutation.java
+++ Permutation.java
@@ -2,36 +2,42 @@ import java.util.Arrays;
 
 public class Permutation {
     public boolean checkInclusion(String s1, String s2) {
-        int lenS1 = s1.length(), lenS2 = s2.length();
-        if (lenS1 > lenS2) {
-            return false;
-        }
-        if (s1 == null || lenS1 == 0) {
+        //condition 1: an empty string is a valid permutation of any string
+        if (s1 == null || s1.length() == 0) {
             return true;
         }
+        //condition 2: If s2 is null or s1 is longer than s2, return false
+        if ((s1 != null && s2 == null) || (s1.length() > s2.length())) {
+            return false;
+        }
+        int lenS1 = s1.length(), lenS2 = s2.length();
 
         char[] charsS1 = s1.toCharArray();
         quickSort(charsS1, 0, charsS1.length - 1);
         String sortedS1 = new String(charsS1);
 
         char[] charsS2 = s2.toCharArray();
+        //The loop iterates over all possible substrings of s2 with length equal to s1
         for (int i = 0; i < lenS2 - lenS1 + 1; i++) {
-            char[] temp = Arrays.copyOfRange(charsS2, i, i + lenS1 - 1);
+            char[] temp = Arrays.copyOfRange(charsS2, i, i + lenS1);// extracted substring with length lenS1
             quickSort(temp, 0, temp.length - 1);
             String sortedSubstring = new String(temp);
             if (sortedSubstring.equals(sortedS1)) {
                 return true;
             }
         }
-        return false;
+        return false;//If no permutation is found after the loop finishes, the method returns false.
     }
 
     //This method performs the recursive quicksort algorithm by dividing the array into smaller subarrays and sorting each.
     public void quickSort(char[] chars, int start, int end) {
+        //Base case: If start is greater than or equal to end, the recursion stops
         if (start >= end) {
             return;
         }
+        //the partition method is called to partition the array around a pivot element
         int pivotIndex = partition(chars, start, end);
+        //After partitioning, the quickSort method recursively sorts the left and right subarray
         quickSort(chars, start, pivotIndex - 1);
         quickSort(chars, pivotIndex + 1, end);
     }
@@ -41,12 +47,11 @@ public class Permutation {
     public int partition(char[] chars, int low, int high) {
         char pivot = chars[low];
         while (low < high) {
-
-            while (low < high && chars[high] > pivot) {
+            while (low < high && chars[high] >= pivot) {
                 high--;
             }
             chars[low] = chars[high];
-            while (low < high && chars[low] < pivot) {
+            while (low < high && chars[low] <= pivot) {
                 low++;
             }
             chars[high] = chars[low];
--- PermutationTest.java
+++ PermutationTest.java
@@ -2,8 +2,23 @@ public class PermutationTest {
     public static void main(String[] args) {
         Permutation p = new Permutation();
         String s1 = "ab";
-        String s2 = "dbaoo";
-        boolean result = p.checkInclusion(s1, s2);
-        System.out.println("One of s1's permutations is the substring of s2: " + result);
+        String s2 = "eidboaoo";
+        boolean result1 = p.checkInclusion(s1, s2);
+        System.out.println("One of s1's permutations is the substring of s2: " + result1);
+
+        String s3 = "ab";
+        String s4 = "eidbaoo";
+        boolean result2 = p.checkInclusion(s3, s4);
+        System.out.println("One of s3's permutations is the substring of s4: " + result2);
+
+        String s5 = "";
+        String s6 = "eidbaoo";
+        boolean result3 = p.checkInclusion(s5, s6);
+        System.out.println("One of s5's permutations is the substring of s6: " + result3);
+
+        String s7 = "ab";
+        String s8 = "";
+        boolean result4 = p.checkInclusion(s7, s8);
+        System.out.println("One of s7's permutations is the substring of s8: " + result4);
     }
 }
